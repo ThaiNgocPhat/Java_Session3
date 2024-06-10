f39370726b725806e9dfc0acd6ce5b036c6920fb
ThaiNgocPhat <thaingocphst231@gmail.com>
2024-06-10 17:11:29 +0700

    update code

--- Exercise7.java
+++
@@ -1,9 +0,0 @@
-package Session3;
-import java.util.Scanner;
-
-public class Exercise7 {
-    public static void main(String[] args) {
-        //khai báo biến để nhập số hàng và số cột
-        
-    }
-}
